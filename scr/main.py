e08b53e8d9895c1d30eea41578aef13a1fdbdb00
philipkarim <54434877+philipkarim@users.noreply.github.com>
2021-06-15 00:53:40 +0200

    just practicing qiskit

---
+++ scr/main.py
@@ -0,0 +1,97 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch.autograd import Function
+from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+import qiskit
+from qiskit import transpile, assemble
+from qiskit.visualization import *
+
+from qiskit import QuantumCircuit, assemble, Aer
+from qiskit.visualization import plot_histogram
+
+
+n = 8
+n_q = n
+n_b = n
+qc_output = QuantumCircuit(n_q,n_b)
+
+for j in range(n):
+    qc_output.measure(j,j)
+
+qc_output.draw()
+
+sim = Aer.get_backend('qasm_simulator')  # this is the simulator we'll use
+qobj = assemble(qc_output)  # this turns the circuit into an object our backend can run
+result = sim.run(qobj).result()  # we run the experiment and get the result from that experiment
+#from the results, we get a dictionary containing the number of times (counts)
+#each result appeared
+counts = result.get_counts()
+#and display it on a histogram
+plot_histogram(counts)
+
+
+qc_encode = QuantumCircuit(n)
+qc_encode.x(7)
+qc_encode.draw()
+
+qc = compose(qc_encode + qc_output)
+qc.draw()
+
+#class QuantumCircuit:
+"""
+    This class provides a simple interface for interaction 
+    with the quantum circuit 
+"""
+"""  
+    def __init__(self, n_qubits, backend, shots):
+        # --- Circuit definition ---
+        self._circuit = qiskit.QuantumCircuit(n_qubits)
+        
+        all_qubits = [i for i in range(n_qubits)]
+        self.theta = qiskit.circuit.Parameter('theta')
+        
+        self._circuit.h(all_qubits)
+        self._circuit.barrier()
+        self._circuit.ry(self.theta, all_qubits)
+        
+        self._circuit.measure_all()
+        # ---------------------------
+
+        self.backend = backend
+        self.shots = shots
+    
+    def run(self, thetas):
+        t_qc = transpile(self._circuit,
+                         self.backend)
+        qobj = assemble(t_qc,
+                        shots=self.shots,
+                        parameter_binds = [{self.theta: theta} for theta in thetas])
+        job = self.backend.run(qobj)
+        result = job.result().get_counts()
+        
+        counts = np.array(list(result.values()))
+        states = np.array(list(result.keys())).astype(float)
+        
+        # Compute probabilities for each state
+        probabilities = counts / self.shots
+        # Get state expectation
+        expectation = np.sum(states * probabilities)
+        
+        return np.array([expectation])
+
+simulator = qiskit.Aer.get_backend('qasm_simulator')
+
+circuit = QuantumCircuit(1, simulator, 100)
+print('Expected value for rotation pi {}'.format(circuit.run([np.pi])[0]))
+circuit._circuit.draw()
+
+"""
