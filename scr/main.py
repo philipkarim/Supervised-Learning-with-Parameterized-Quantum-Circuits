362c736a806b229214262aed89475500a7b3daac
philipniane <philipkarim1998@hotmail.com>
2021-06-15 09:45:06 +0200

    remember to print instead of drawing circuit

--- scr/main.py
+++ scr/main.py
@@ -28,7 +28,7 @@ for j in range(n):
     qc_output.measure(j,j)
 
 qc_output.draw()
-
+plt.show()
 sim = Aer.get_backend('qasm_simulator')  # this is the simulator we'll use
 qobj = assemble(qc_output)  # this turns the circuit into an object our backend can run
 result = sim.run(qobj).result()  # we run the experiment and get the result from that experiment
@@ -38,14 +38,17 @@ counts = result.get_counts()
 #and display it on a histogram
 plot_histogram(counts)
 
+plt.show()
+print(qc_output)
 
+"""
 qc_encode = QuantumCircuit(n)
 qc_encode.x(7)
 qc_encode.draw()
 
 qc = compose(qc_encode + qc_output)
 qc.draw()
-
+"""
 #class QuantumCircuit:
 """
     This class provides a simple interface for interaction 
